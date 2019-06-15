04879934a8c553f4eea266193bfbce22562a4bf7
duchuy1091997 <duchuy1091997@gmail.com>
2019-06-15 20:46:41 +0700

    Sap xong

--- src/main.js
+++ src/main.js
@@ -1,11 +1,26 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import jQuery from 'jquery'
+import 'popper.js'
+import 'bootstrap'
+import './assets/app.scss'
 import './registerServiceWorker'
+import {fb} from './firebase'
 
+window.$ = window.jQuery = jQuery;
+
+Vue.component('Navbar', require('./components/Navbar.vue').default)
 Vue.config.productionTip = false
 
-new Vue({
-  router,
-  render: h => h(App)
-}).$mount('#app')
+let app = '';
+
+fb.auth().onAuthStateChanged(function(user) {
+  if(!app){
+    new Vue({
+      router,
+      render: h => h(App)
+    }).$mount("#app");
+    
+  }
+});
